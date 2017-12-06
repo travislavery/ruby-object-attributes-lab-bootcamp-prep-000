0b0bdfa79ffad5ddc8911004ddc5c64941d95277
Travis Lavery <travisalavery@gmail.com>
2017-12-06 01:27:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,9 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
-  def breed(breed)
-    @breed = breed
+  def breed
+    @breed
   end
-  def breed=()
+  def breed=(breed)
+    @breed = breed
 end
